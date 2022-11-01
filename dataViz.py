743aea2a42ad709901a7faba5bcbeb346930bd32
Samiksha Arora <arora21@illinois.edu>
2022-11-01 00:27:07 -0500

    data visualization updates

--- dataViz.py
+++ dataViz.py
@@ -1,9 +1,7 @@
-import pandas
 import json
 import matplotlib.pyplot as plt
-import numpy as nnp
-import seaborn as sns
-from pyspc import *
+import pandas as pd  
+
 
 diameter_dates = []
 diameter_avgs = []
@@ -16,3 +14,34 @@ with open("example_data.json") as f:
     diameter_avgs = load_data["Diameter"]["avgs"]
 
 
+all_dates = []
+
+for key in diameter_dates:
+    all_dates.append(key.split('T')[0])
+
+df = zip(all_dates, diameter_avgs)
+df_new = pd.DataFrame(df, columns=('date', 'avg'))
+
+df_new['USL']=diameter_USL
+df_new['LSL']=diameter_LSL
+df_new['NOM']=diameter_NOM
+
+xdf = df_new.groupby("date", as_index=False).mean() 
+print (xdf)
+xpoints = xdf['date'].tolist()
+ypoints = xdf['avg'].tolist()
+plt.axhline(y=diameter_USL, color='r', linestyle='-')
+plt.axhline(y=diameter_LSL, color='r', linestyle='-')
+plt.xticks(rotation=20)
+plt.xlabel("Date")
+plt.ylabel("Diameter averages")
+plt.title("Historic data visualization for diameter")
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+
+
+
+
+    
