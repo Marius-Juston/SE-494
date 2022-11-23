5fad20219380fe826da25aa0622670daf014edf2
Marius-Juston <Marius.juston@hotmail.fr>
2022-11-23 15:34:42 -0600

    UDP server sending random data

--- udp_server.py
+++ udp_server.py
@@ -1,18 +1,16 @@
 import socket
 
+import numpy as np
+
 localIP = "127.0.0.1"
 
 localPort = 20000
 
 bufferSize = 1024
 
-msgFromServer = "Hello UDP Client"
-
-bytesToSend = str.encode(msgFromServer)
-
 # Create a datagram socket
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
 # Bind to address and ip
 
@@ -21,6 +19,9 @@ UDPServerSocket.bind((localIP, localPort))
 print("UDP server up and listening")
 
 # Listen for incoming datagrams
+
+num_datapoints = 10
+num_columns = 3
 
 while (True):
     # bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -36,5 +37,10 @@ while (True):
     # print(clientIP)
 
     # Sending a reply to client
+
+    msgFromServer = ",".join(map(str, np.random.random(num_columns * num_datapoints)))
+
+    bytesToSend = str.encode(msgFromServer)
+    print(bytesToSend)
 
     UDPServerSocket.sendto(bytesToSend, ("127.0.0.1", localPort + 1))
